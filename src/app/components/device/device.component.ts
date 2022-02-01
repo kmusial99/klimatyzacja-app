bdf6f81ddfe44b03c0ae5076991dffa6798e0103
karol-musial <nYmina:karol-musial>
2022-02-02 00:28:14 +0100

    dodanie powiadomień

--- src/app/components/device/device.component.ts
+++ src/app/components/device/device.component.ts
@@ -2,13 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Urzadzenie } from '../../models/urzadzenie';
 import { UrzadzenieService } from '../../service/urzadzenieService';
-import { ConfirmationService, ConfirmEventType, Message, MessageService } from 'primeng/api';
+import { ConfirmationService, Message, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
   styleUrls: ['./device.component.scss'],
-  providers: [ConfirmationService,MessageService]
+  providers: [ConfirmationService, MessageService]
 })
 export class DeviceComponent implements OnInit, OnDestroy {
 
@@ -53,14 +53,32 @@ export class DeviceComponent implements OnInit, OnDestroy {
   }
 
   save() {
+    // if (this.deviceToUpdate.czyZaplanowaneWlaczenie) {
+    //   this.setToEnableMessageShow();
+    // }
+    // if (this.deviceToUpdate.wlaczone) {
+    //   this.turnOnMessageShow();
+    // }
     this.deviceToUpdate.dataPlanowanegoWlaczenia.setSeconds(0);
     this.urzadzenieService.updateUrzadzenie(this.deviceToUpdate);
     this.router.navigate(['home']);
   }
 
-  addMessages() {
+  setToEnableMessageShow() {
+    const year = new Intl.DateTimeFormat('pl', { year: 'numeric' }).format(this.deviceToUpdate.dataPlanowanegoWlaczenia);
+    const month = new Intl.DateTimeFormat('pl', { month: '2-digit' }).format(this.deviceToUpdate.dataPlanowanegoWlaczenia);
+    const day = new Intl.DateTimeFormat('pl', { day: '2-digit' }).format(this.deviceToUpdate.dataPlanowanegoWlaczenia);
+    const hour = new Intl.DateTimeFormat('pl', { hour: '2-digit' }).format(this.deviceToUpdate.dataPlanowanegoWlaczenia);
+    const minute = new Intl.DateTimeFormat('pl', { minute: '2-digit' }).format(this.deviceToUpdate.dataPlanowanegoWlaczenia);
+    const formattedDate = `${day}.${month}.${year} o godzinie ${hour}:${minute}`;
     this.messages = [
-      { severity: 'success', summary: 'Zaplanowano uruchomienie', detail: '' }
+      { severity: 'success', summary: `Klimatyzator uruchomi się ${formattedDate}` }
+    ];
+  }
+
+  turnOnMessageShow() {
+    this.messages = [
+      { severity: 'success', summary: 'Uruchomiono klimatyzator' }
     ];
   }
 
@@ -82,7 +100,7 @@ export class DeviceComponent implements OnInit, OnDestroy {
       header: 'Usuwanie urządzenia',
       icon: 'pi pi-info-circle',
       accept: () => {
-        this.messages = [ { severity: 'success', summary: 'Usunięto urządzenie'} ];
+        this.messages = [{ severity: 'success', summary: 'Usunięto urządzenie' }];
         this.router.navigate(['home']);
         this.urzadzenieService.deleteDevice(this.urzadzenie);
       }
